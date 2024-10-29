72fc0a3a758952acefb06269612aef266894ad91
htoohtoo <htoohtooaung.cloud@gmail.com>
2024-10-29 16:45:57 +0000

    Modify terraform files

--- tf-vault-setup/outputs.tf
+++ tf-vault-setup/outputs.tf
@@ -33,3 +33,9 @@ output "vault_auth_backend_approle_path" {
   description = "This is AppRole Role Path"
   value       = vault_auth_backend.approle_path.path
 }
+
+# Output the Trusted-Entity AppRole Backend path
+output "vault_auth_backend_approle_path_trusted_entity" {
+  description = "This is AppRole Role Path"
+  value       = vault_approle_auth_backend_role.trusted_entity_approle_role.backend
+}
