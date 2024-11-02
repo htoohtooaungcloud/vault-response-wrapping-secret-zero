e6b2319d188ec61934b6a63a9f50ad94e0a8a14d
htoohtoo <htoohtooaung.cloud@gmail.com>
2024-11-02 13:43:48 +0000

    Modify vault trusted-entity role and add manage db

--- tf-vault-setup/main.tf
+++ tf-vault-setup/main.tf
@@ -43,12 +43,12 @@ resource "vault_auth_backend" "approle_path" {
 resource "vault_approle_auth_backend_role" "trusted_entity_approle_role" {
   backend               = vault_auth_backend.approle_path.path
   role_name             = var.trusted_entity_policy_approle_role_name
-  token_num_uses        = 3    # Usage number of the token that generated after authenticated to Vault; Noted Count plus 1
+  token_num_uses        = 2    # Usage number of the token that generated after authenticated to Vault; Noted Count plus 1
   secret_id_num_uses    = 0    # Can use the secret-id infinitely
   token_ttl             = 1440 # Suggest to check how often required to build 
   token_max_ttl         = 4320
-  token_bound_cidrs     = ["172.19.0.5/32"] # Could it be Jenkins Server's IP Address or Load-Balancer's IP Address as LIST
-  secret_id_bound_cidrs = ["172.19.0.5/32"] # Could it be Jenkins Server's IP Address or Load-Balancer's IP Address as LIST
+  token_bound_cidrs     = ["172.21.0.3/32"] # Could it be Jenkins Server's IP Address or Load-Balancer's IP Address as LIST
+  secret_id_bound_cidrs = ["172.21.0.3/32"] # Could it be Jenkins Server's IP Address or Load-Balancer's IP Address as LIST
   token_policies        = [vault_policy.trusted_entity_policy.name]
   depends_on  = [vault_auth_backend.approle_path]
 }
